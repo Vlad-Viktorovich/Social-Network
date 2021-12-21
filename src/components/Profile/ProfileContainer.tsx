175168fb915cd99cdb3da805d574e50857b4a1f0
VladViltorovich <vizachem@gmail.com>
2021-12-21 19:13:53 +0300

    add withRouter

--- src/components/Profile/ProfileContainer.tsx
+++ src/components/Profile/ProfileContainer.tsx
@@ -1,21 +1,32 @@
 import React from 'react';
-
 import {Profile} from './Profile';
 import axios from 'axios';
 import {connect} from 'react-redux';
 import {ProfileResponsePropsType, setUserProfile} from '../../redux/profile-reducer';
-
 import {RootStoreType} from '../../redux/redux-store';
+import {RouteComponentProps, withRouter} from 'react-router-dom';
 
-export type ProfileContainerPropsType = {
-    profile: ProfileResponsePropsType
-    setUserProfile:(data:ProfileResponsePropsType) => void
+export type ProfileContainerPropsType = MapStatePropsType & MapDispatchPropsType
+type MathType={
+    userId:string
 }
+type MapStatePropsType ={
+    profile: ProfileResponsePropsType
+}
+type MapDispatchPropsType ={
+    setUserProfile: (data: ProfileResponsePropsType) => void
+}
+type PropsType = RouteComponentProps<MathType> & ProfileContainerPropsType
 
-class ProfileContainer extends React.Component<ProfileContainerPropsType> {
+
+class ProfileContainer extends React.Component<PropsType> {
 
     componentDidMount() {
-        axios.get<ProfileResponsePropsType>(`https://social-network.samuraijs.com/api/1.0/profile/2`)
+        let userId = this.props.match.params.userId
+        if (!userId) {
+            userId = "2"
+        }
+        axios.get<ProfileResponsePropsType>(`https://social-network.samuraijs.com/api/1.0/profile/2` + userId)
             .then(response => {
                 this.props.setUserProfile(response.data)
 
@@ -29,10 +40,11 @@ class ProfileContainer extends React.Component<ProfileContainerPropsType> {
 
     }
 }
-let mapStateToProps = (state:RootStoreType) => ({
-    profile:state.profilePage.profile
+
+let mapStateToProps = (state: RootStoreType):MapStatePropsType => ({
+    profile: state.profilePage.profile
 })
 
+let withUrlDataContainerComponent = withRouter(ProfileContainer)
 
-
-export default connect (mapStateToProps, {setUserProfile})(ProfileContainer);
+export default connect(mapStateToProps, {setUserProfile})(withUrlDataContainerComponent);
